f684b6c704023a9899cc7d008c7d4c1163e7a39c
mohamedmabrouk582 <mabroukmohamed582@gmail.com>
2023-04-25 16:37:40 +0400

    upgrade jdk

--- app/src/main/java/com/example/bitriseapp/MainActivity.kt
+++ app/src/main/java/com/example/bitriseapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -31,10 +33,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Card(modifier = Modifier.fillMaxWidth()) {
+
+    }
 }
 
 @Preview(showBackground = true)
