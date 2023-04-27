64edc8b9044ed6929945ecad4414cc989db3ce3b
mohamedmabrouk582 <mabroukmohamed582@gmail.com>
2023-04-27 11:17:27 +0400

    add google services

--- app/src/main/java/com/example/bitriseapp/MainActivity.kt
+++ app/src/main/java/com/example/bitriseapp/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.bitriseapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bitriseapp.ui.theme.BitriseAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +41,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Card(modifier = Modifier.fillMaxWidth()) {
-
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = ""
+            )
+        }
     }
 }
 
