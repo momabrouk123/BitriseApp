7842bed7bc209b4adb14a374e9c3fd7062a93673
momabrouk123 <130565101+momabrouk123@users.noreply.github.com>
2023-05-12 10:24:42 +0400

    add first test case

--- app/src/androidTest/java/com/example/bitriseapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/bitriseapp/MainActivityTest.kt
@@ -4,10 +4,13 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
     @get:Rule
