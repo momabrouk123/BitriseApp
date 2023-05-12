01fd5f275b1b5f123be0459cbb8ce5175d6a6894
momabrouk123 <130565101+momabrouk123@users.noreply.github.com>
2023-05-12 16:14:50 +0400

    add first test case

--- app/src/androidTest/java/com/example/bitriseapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/bitriseapp/MainActivityTest.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
@@ -12,17 +14,25 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @get:Rule
-    val rule = createComposeRule()
+//    @get:Rule
+//    val rule = createComposeRule()
+//    @Test
+//    fun first_test(){
+//        rule.setContent {
+//            Greeting()
+//        }
+//
+//        rule.onNodeWithText("1").performClick()
+//
+//        rule.onNodeWithText("1").assertExists()
+//    }
+
+
     @Test
-    fun first_test(){
-        rule.setContent {
-            Greeting()
-        }
-
-        rule.onNodeWithText("1").performClick()
-
-        rule.onNodeWithText("1").assertExists()
-    }
+     fun useAppContext() {
+        // Context of the app under test.
+         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+         Assert.assertEquals("com.example.bitriseapp", appContext.packageName)
+         }
 
 }
