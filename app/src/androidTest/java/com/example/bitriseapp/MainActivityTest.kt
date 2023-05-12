73424cce71476658e3a2c02ceb34355640a52de7
momabrouk123 <130565101+momabrouk123@users.noreply.github.com>
2023-05-12 11:45:14 +0400

    add first test case

--- app/src/androidTest/java/com/example/bitriseapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/bitriseapp/MainActivityTest.kt
@@ -1,29 +1,16 @@
 package com.example.bitriseapp
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @get:Rule
-    val rule = createComposeRule()
-
     @Test
     fun first_test(){
-        rule.setContent {
-            GreetingPreview()
-        }
-
-        rule.onNodeWithText("Pause").performClick()
-
-        rule.onNodeWithText("Pause").assertExists()
+        assertEquals(4, 2 + 2)
     }
+
 }
